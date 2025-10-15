1737367c2336d9ab15c3980b05403dda8d84c993
Upsided87 <thibaultboissieres@gmail.com>
2025-10-15 15:02:24 +0200

    Ajout scipt js

--- JAVASCRIPT/main.js
+++ JAVASCRIPT/main.js
@@ -1,1 +1,10 @@
- 
+function masquer(x) {
+    const matches = document.querySelectorAll(x);
+    matches.forEach(element => {
+        if (element.style.display === "none") {
+            element.style.display = "table-row";
+        } else {
+            element.style.display = "none";
+        }
+    });
+}
