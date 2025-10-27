312c1ab90eeed7e9f43879d36eb1ed13922c945e
Upsided87 <thibaultboissieres@gmail.com>
2025-10-27 15:08:59 +0100

    Refactor code structure for improved readability and maintainability

--- JAVASCRIPT/main.js
+++ JAVASCRIPT/main.js
@@ -2,6 +2,19 @@ function masquer(classe) {
     const elements = document.querySelectorAll(classe);
     elements.forEach(element => {
         element.classList.toggle("masque");
-        });
-    };
+    });
+}
 
+const logo = document.querySelector('.nav-logo');
+const links = document.querySelector('.nav-links');
+const navbarLeft = document.querySelector('.navbar-left');
+
+// Quand on entre sur le logo, ajoute la classe "active"
+logo.addEventListener('mouseenter', () => {
+    links.classList.add('active');
+});
+
+// Quand on sort du bloc navbar-left, retire la classe "active"
+navbarLeft.addEventListener('mouseleave', () => {
+    links.classList.remove('active');
+});
